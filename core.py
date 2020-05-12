5d231c1e7488c2a70f94e6929d6de5e7b2a8f3fd
maciejjablonsky <maciekjablonsky@gmail.com>
2020-05-12 18:02:39 +0200

    init commit

---
+++ core.py
@@ -0,0 +1,26 @@
+import math
+import random
+import algorithms
+
+
+def encrypt_file(input_file , output_file, publicKey):
+    with open(input_file, "rb") as text_file:
+        bytes_stream = text_file.read()
+        encoded = algorithms.encrypt_rsa(bytes_stream, publicKey)
+
+        with open(output_file, 'wb+') as encoded_file:
+            [encoded_file.write(x) for x in encoded]
+
+
+def decrypt_file(input_file : str, output_file: str, privateKey):
+    with open(input_file,"rb") as encoded_file:
+        encoded = []
+        
+        #creating list of unicode values
+        # [encoded.append(int(encoded_file.read(4))) for x in ]
+
+        decoded = algorithms.decrypt_rsa(encoded, privateKey)
+        with open(output_file,'wb+') as decoded_file:
+            
+            [decoded_file.w]
+            decoded_file.write(decoded)
--- main.py
+++ main.py
@@ -1,43 +1,23 @@
-import math
-import random
 import sympy as sy
+import core
+import algorithms
 
-# lower common multiplier
-def lcm(a, b):
-    return int(a*b / math.gcd(a, b))
+ENCRYPT = True
+DECRYPT = False
 
-
-def modInverse(a, m):
-    for x in range(1, m):
-        if (a * x) % m == 1:
-            return x
-    return 1 
-
-
-def rsa(p, q): 
-    n = p * q
-    phi = lcm(p - 1, q - 1)
-
-    shuffleArray = list(range(2, phi))
-    random.shuffle(shuffleArray)
-
-    for e in shuffleArray:  
-        if math.gcd(e, phi) == 1:
-            break
-
-    d = modInverse(e, phi)
-
-    print("Klucz publiczny : (" + str(n) + "," + str(e) + ")")
-    print("Klucz prywatny : (" + str(n) + "," + str(d) + ")")
-
-
-p = sy.randprime(1e2, 1e4)
-q = sy.randprime(1e2, 1e4)
-
+p = sy.randprime(1e2, 1e3)
+q = sy.randprime(1e2, 1e3)
 while q == p:
-    q = sy.randprime(1e4, 1e8)
+    q = sy.randprime(1e2, 1e4)
 
-rsa(p, q)
+publicKey, privateKey = algorithms.get_keys(p, q)
+
+print(publicKey)
+print(privateKey)
 
 
+if ENCRYPT:
+    core.encrypt_file("dupa.txt","dupa_encoded.txt",publicKey)
 
+if DECRYPT:
+    core.decrypt_file("dupa_encoded.txt","dupa_decoded.txt",privateKey)
